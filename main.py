2ee02fb68781c53c0b382e3d6c80f5deae2ed47a
RodrigoCabrera534 <n00325552@upn.pe>
2026-06-19 12:27:28 -0500

    Parte 1: funcion bienvenida

---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py - Archivo principal
+from utils import bienvenida
+
+# Llamada a la función de bienvenida
+bienvenida()
---
+++ utils.py
@@ -0,0 +1,12 @@
+# utils.py - Módulo de funciones reutilizables
+# Laboratorio 6 - Fundamentos de Programación
+
+def bienvenida():
+    """
+    Función sin parámetros que muestra un mensaje de bienvenida.
+    Retorno: None
+    """
+    print("=" * 45)
+    print("  Bienvenido al Sistema de Funciones - UPN ")
+    print("  Fundamentos de Programación  2026-1    ")
+    print("=" * 45)
